8481fc495d980cb050fd75d92a6ee28eb5970616
Abdeljalil Nadiri <nadiri@najasoft.com>
2019-03-14 20:27:50 +0100

    the module IN is no longer available in python 3.7 standard libraries.

--- foundations-of-python-network-programming/python3/02/big_sender.py
+++ foundations-of-python-network-programming/python3/02/big_sender.py
@@ -2,24 +2,27 @@
 # Foundations of Python Network Programming - Chapter 2 - big_sender.py
 # Send a big UDP packet to our server.
 
-import IN, socket, sys
+import socket, sys
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 MAX = 65535
 PORT = 1060
-
+#values retrieved from in.h
+IP_MTU_DISCOVER=10  # Set/get path MTU discover state.
+IP_PMTUDISC_DO=2  #IP_PMTUDISC_DO will force all outgoing packets to have the DF bit set and an attempt to send packets larger than path MTU will result in an error. 
+IP_MTU=14
 if len(sys.argv) != 2:
     print('usage: big_sender.py host', file=sys.stderr)
     sys.exit(2)
 
 hostname = sys.argv[1]
 s.connect((hostname, PORT))
-s.setsockopt(socket.IPPROTO_IP, IN.IP_MTU_DISCOVER, IN.IP_PMTUDISC_DO)
+s.setsockopt(socket.IPPROTO_IP, IP_MTU_DISCOVER, IP_PMTUDISC_DO)
 try:
+    print('MTU:', s.getsockopt(socket.IPPROTO_IP, IP_MTU))
     s.send(b'#' * 65000)
 except socket.error:
     print('The message did not make it')
-    option = getattr(IN, 'IP_MTU', 14)  # constant taken from <linux/in.h>
-    print('MTU:', s.getsockopt(socket.IPPROTO_IP, option))
+    print('MTU:', s.getsockopt(socket.IPPROTO_IP, IP_MTU))
 else:
     print('The big message was sent! Your network supports really big packets!')
